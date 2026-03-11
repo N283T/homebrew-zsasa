dd1bc391068edb9ee0b432160da48fa3f0be4463
N283T <w255508b@yokohama-cu.ac.jp>
2026-03-11 22:26:50 +0900

    feat: add zsasa formula v0.2.4

---
+++ Formula/zsasa.rb
@@ -0,0 +1,35 @@
+class Zsasa < Formula
+  desc "Fast Solvent Accessible Surface Area (SASA) calculator"
+  homepage "https://n283t.github.io/zsasa/"
+  license "MIT"
+  version "0.2.4"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/N283T/zsasa/releases/download/v#{version}/zsasa-#{version}-macos-aarch64"
+      sha256 "d25ff0904ef1300bc7b69affab780061055e0c5bf7f9f180983a708d381497a9"
+    else
+      url "https://github.com/N283T/zsasa/releases/download/v#{version}/zsasa-#{version}-macos-x86_64"
+      sha256 "97fdae690982d32bf5b6e7d78300480629a8d173acf55c9c0f849d038eeb44e9"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/N283T/zsasa/releases/download/v#{version}/zsasa-#{version}-linux-aarch64"
+      sha256 "82e99edaef90906fc414de35aee1529649efce50fe82f06510a46c490946ff3e"
+    else
+      url "https://github.com/N283T/zsasa/releases/download/v#{version}/zsasa-#{version}-linux-x86_64"
+      sha256 "250c660de9d0de3f2ccb9e30c6aa8244ff5bb1ea92ac814549f27de1a71a3306"
+    end
+  end
+
+  def install
+    binary_name = "zsasa-#{version}-#{OS.mac? ? "macos" : "linux"}-#{Hardware::CPU.arm? ? "aarch64" : "x86_64"}"
+    bin.install binary_name => "zsasa"
+  end
+
+  test do
+    assert_match "zsasa", shell_output("#{bin}/zsasa --version")
+  end
+end
